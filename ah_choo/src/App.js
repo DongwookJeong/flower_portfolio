e88d559a0f27cbb4959f8df85889a5c2e6e890cc
DongwookJeong <nolhoon211@gmail.com>
2023-07-03 19:51:29 +0900

    지역별 태그의 공통점이 많고 각각 나누어 놓을 이유가 타당하지 않다고 판단,  App.js에 담고 진행하기로 결정후 변경

--- ah_choo/src/App.js
+++ ah_choo/src/App.js
@@ -1,23 +1,27 @@
 import styled from "styled-components";
 
-import WholeArea from "./components/Nationwide";
-import SeoulArea from "./components/Seoul";
-import GyeonggiArea from "./components/Gyeonggi";
-import IncheonArea from "./components/Incheon";
-import GangwonArea from "./components/Gangwon";
-import ChungnamArea from "./components/Chungnam";
-import SejongArea from "./components/Sejong";
-import DaejeonArea from "./components/DaeJeon";
-import ChungbukArea from "./components/Chungbuk";
-import GyeongbukArea from "./components/Gyeongbuk";
-import DaeguArea from "./components/Daegu";
-import JeonbukArea from "./components/Jeonbuk";
-import JeonnamArea from "./components/Jeonnam";
-import GwangjuArea from "./components/Gwangju";
-import GyeongnamArea from "./components/Gyeongnam";
-import UlsanArea from "./components/Ulsan";
-import BusanArea from "./components/Busan";
-import JejuArea from "./components/Jeju";
+// * img 공통 스타일
+import "./App.scss";
+
+// ! 마크업을 위한 이미지 파일 삭제 예정
+import seoul from "./image/seoul_green.png";
+import gyeonggi from "./image/gyeonggi_red.png";
+import chungbuk from "./image/chungbuk_orange.png";
+import chungnam from "./image/chungnam_green.png";
+import daegu from "./image/daegu_red.png";
+import daejeon from "./image/daejeon_red.png";
+import gangwon from "./image/gangwon_orange.png";
+import gwangju from "./image/gwangju_yellow.png";
+import gyeongbuk from "./image/gyeongbuk_green.png";
+import gyeongnam from "./image/gyeongnam_orange.png";
+import incheon from "./image/incheon_orange.png";
+import jeju from "./image/jeju_red.png";
+import jeonbuk from "./image/jeonbuk_yellow.png";
+import jeonnam from "./image/jeonnam_red.png";
+import sejong from "./image/sejong_yellow.png";
+import ulsan from "./image/ulsan_yellow.png";
+import busan from "./image/busan_red.png";
+
 const AppStyle = styled.div`
   width: 100vw;
 
@@ -27,24 +31,23 @@ const AppStyle = styled.div`
 const App = () => {
   return (
     <AppStyle>
-      <WholeArea />
-      <SeoulArea />
-      <GyeonggiArea />
-      <IncheonArea />
-      <GangwonArea />
-      <ChungnamArea />
-      <SejongArea />
-      <DaejeonArea />
-      <ChungbukArea />
-      <GyeongbukArea />
-      <DaeguArea />
-      <JeonbukArea />
-      <JeonnamArea />
-      <GwangjuArea />
-      <GyeongnamArea />
-      <UlsanArea />
-      <BusanArea />
-      <JejuArea />
+      <img className="seoul" src={seoul} />
+      <img className="gyeonggi" src={gyeonggi} />
+      <img className="incheon" src={incheon} />
+      <img className="gangwon" src={gangwon} />
+      <img className="chungnam" src={chungnam} />
+      <img className="sejong" src={sejong} />
+      <img className="daejeon" src={daejeon} />
+      <img className="chungbuk" src={chungbuk} />
+      <img className="gyeongbuk" src={gyeongbuk} />
+      <img className="daegu" src={daegu} />
+      <img className="gyeongnam" src={gyeongnam} />
+      <img className="busan" src={busan} />
+      <img className="ulsan" src={ulsan} />
+      <img className="jeonnam" src={jeonnam} />
+      <img className="jeonbuk" src={jeonbuk} />
+      <img className="gwangju" src={gwangju} />
+      <img className="jeju" src={jeju} />
     </AppStyle>
   );
 };
