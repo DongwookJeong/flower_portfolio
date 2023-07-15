8d1a85d45b2590d3319ff1bb886cfee63f3dff5a
DongwookJeong <nolhoon211@gmail.com>
2023-07-15 17:24:49 +0900

    파일 정리

--- ah_choo/client/src/App.js
+++ ah_choo/client/src/App.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 // * img 공통 스타일
 import "./App.scss";
-
+/*
 // ! 마크업을 위한 이미지 파일 삭제 예정
 import seoul from "./image/seoul_green.png";
 import gyeonggi from "./image/gyeonggi_red.png";
@@ -21,7 +22,7 @@ import jeonnam from "./image/jeonnam_red.png";
 import sejong from "./image/sejong_yellow.png";
 import ulsan from "./image/ulsan_yellow.png";
 import busan from "./image/busan_red.png";
-
+*/
 const AppStyle = styled.div`
   width: 100vw;
 
@@ -29,25 +30,51 @@ const AppStyle = styled.div`
 `;
 
 const App = () => {
+  const [mapList, setMapList] = useState(null);
+
+  const fetchData = () => {
+    fetch("http://localhost:5000/api/mapimage")
+      .then((response) => response.json())
+      .then((data) => setMapList(data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  /*
+  const dotdot = () => {
+    fetch("http://localhost:5000/api/mapimage", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        red,
+        orange,
+        yellow,
+        green,
+      }),
+    }).then(() => fetchData());
+  };
+  */
   return (
     <AppStyle>
-      <img className="seoul" src={seoul} />
-      <img className="gyeonggi" src={gyeonggi} />
-      <img className="incheon" src={incheon} />
-      <img className="gangwon" src={gangwon} />
-      <img className="chungnam" src={chungnam} />
-      <img className="sejong" src={sejong} />
-      <img className="daejeon" src={daejeon} />
-      <img className="chungbuk" src={chungbuk} />
-      <img className="gyeongbuk" src={gyeongbuk} />
-      <img className="daegu" src={daegu} />
-      <img className="gyeongnam" src={gyeongnam} />
-      <img className="busan" src={busan} />
-      <img className="ulsan" src={ulsan} />
-      <img className="jeonnam" src={jeonnam} />
-      <img className="jeonbuk" src={jeonbuk} />
-      <img className="gwangju" src={gwangju} />
-      <img className="jeju" src={jeju} />
+      <img className="seoul" />
+      <img className="gyeonggi" />
+      <img className="incheon" />
+      <img className="gangwon" />
+      <img className="chungnam" />
+      <img className="sejong" />
+      <img className="daejeon" />
+      <img className="chungbuk" />
+      <img className="gyeongbuk" />
+      <img className="daegu" />
+      <img className="gyeongnam" />
+      <img className="busan" />
+      <img className="ulsan" />
+      <img className="jeonnam" />
+      <img className="jeonbuk" />
+      <img className="gwangju" />
+      <img className="jeju" />
     </AppStyle>
   );
 };
